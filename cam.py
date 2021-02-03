74d77d0dad646732ffa9a3337fd7d22f8cb6ac24
eunhyeok <eunhyeok8903@gmail.com>
2021-02-03 13:54:21 +0900

    # LBPH학습

--- cam.py
+++ cam.py
@@ -8,7 +8,6 @@ cap = cv2.VideoCapture(0)
 cap.set(3, 640)  # set Width
 cap.set(4, 480)  # set Height
 while (True):
-
     ret, frame = cap.read()
     frame = cv2.flip(frame, 1)  # Flip camera vertically //상하 반전 0,1
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
